1472b389d032449837e4c9129b40f746f833799b
Jose Lopez <joselopez11394@gmail.com>
2020-04-15 21:22:26 -0400

    created length and name method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,5 +1,8 @@
 class Ship
+  attr_accessor :length
+  attr_reader :name
   def initialize(name, length)
-
+    @name = name
+    @length = length
   end
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,7 +2,16 @@ require "rspec/autorun"
 require_relative "../lib/ship.rb"
 
 describe Ship do
+  let(:ship) {Ship.new("cruiser", 3)}
   it "should exist" do
     expect(Ship.new("cruiser", 3)).to be_kind_of(Ship)
+  end
+
+  it "should return ship name" do
+    expect(ship.name).to eq("cruiser")
+  end
+
+  it "should return length of ship" do
+    expect(ship.length).to eq(3)
   end
 end
