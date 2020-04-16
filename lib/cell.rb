18aa4922d2388ca06b7bf0ea3558d4abe888ce28
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-16 12:28:42 -0400

    created empty? class and test

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,5 +2,14 @@ class Cell
   attr_reader :coordinate
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+  end
+
+  def empty?
+    if @ship == nil
+      return true
+    else
+      return false
+    end
   end
 end
