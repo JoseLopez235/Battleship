674803806956811391225defbbc1765466bcc512
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-16 10:16:13 -0400

    coordinate method and test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,10 @@
 class Cell
+  attr_reader :coordinate
   def initialize(coordinate)
+    @coordinate = coordinate
+  end
 
+  def coordinate
+  @coordinate
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,7 +2,12 @@ require "rspec/autorun"
 require_relative "../lib/cell.rb"
 
 describe Cell do
+  let(:cell) {Cell.new("B4")}
   it "should exist" do
-    expect(Cell.new("B4")).to be_kind_of(Cell)
+    expect(cell).to be_kind_of(Cell)
+  end
+
+  it "should return the coordinate for the cell" do
+    expect(cell.coordinate).to eq("B4")
   end
 end
