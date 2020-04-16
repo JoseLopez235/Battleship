3f1008b8267006bfe2cc054924c3cbc5ec8b3f71
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-16 13:25:26 -0400

    fixed conflict

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,16 +5,15 @@ class Cell
     @ship = nil
   end
 
-<<<<<<< HEAD
+  def ship
+    return @ship
+  end
+
   def empty?
     if @ship == nil
       return true
     else
       return false
     end
-=======
-  def ship
-    return @ship
->>>>>>> d3e4d1d9aa62f1bd7bd31915f75048b2f98de104
   end
 end
