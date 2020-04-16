2b6984f6d3f1dada262523ec931cb1b08604960a
Jose Lopez <joselopez11394@gmail.com>
2020-04-16 12:22:46 -0400

    made fired_upon? method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,6 +1,13 @@
+require_relative 'ship.rb'
+
 class Cell
   attr_reader :coordinate
   def initialize(coordinate)
     @coordinate = coordinate
+    @fired_upon_bool = false
+  end
+
+  def fired_upon?
+    @fired_upon_bool
   end
 end
