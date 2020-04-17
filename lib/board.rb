683f092a7ae6e92b392b087e0c066973e465113c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-17 15:43:25 -0400

    finished place method

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,21 @@
+require_relative 'cell.rb'
+
 class Board
   attr_accessor :cells
   def initialize
-    @cells = {}
+    @cells = {"A1" => "A1", "A2" => "A2", "A3" => "A3"}
   end
 
   def valid_coordinate?(coord)
     @cells.include?(coord)
   end
+
+  def place(ship,coords)
+    if valid_placement?
+      coords.each do |ele|
+        @cells[ele] = ship
+      end
+    end
+  end
+
 end
