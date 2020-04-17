d13d21b7be47815e545d8a5f9b3113bca4160018
Miguel Silva <msilvaale08@gmail.com>
2020-04-17 17:36:03 -0400

    rendering cell

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -4,6 +4,8 @@ require_relative "../lib/ship.rb"
 
 describe Cell do
   let(:cell) {Cell.new("B4")}
+  let(:cell_two) {Cell.new("B3")}
+  let(:cell_three) {Cell.new("B2")}
   let(:ship) {Ship.new("cruiser", 3)}
   it "should exist" do
     expect(cell).to be_kind_of(Cell)
@@ -40,14 +42,37 @@ describe Cell do
     expect(ship.health).to eq(2)
   end
 
-  it "should return a string representation if ship has been fired upon." do
+  it "should return a string with a dot" do
+    expect(cell.render).to eq(".")
+  end
+
+  it "should return a string with an M." do
+    cell.fire_upon
+
+    expect(cell.render).to eq("M")
+  end
+
+  it "should return a string with an H." do
     cell.place_ship(ship)
     cell.fire_upon
 
-    expect(cell.render).to eq(".")
-    expect(cell.render).to eq("M")
     expect(cell.render).to eq("H")
-    expect(cell.render).to eq("X")
+  end
+
+  it "should return a string with an S." do
+    cell.place_ship(ship)
+
     expect(cell.render(true)).to eq("S")
+  end
+
+  it "should return a string with an X." do
+    cell.place_ship(ship)
+    cell_two.place_ship(ship)
+    cell_three.place_ship(ship)
+    cell.fire_upon
+    cell_two.fire_upon
+    cell_three.fire_upon
+
+    expect(cell.render).to eq("X")
   end
 end
