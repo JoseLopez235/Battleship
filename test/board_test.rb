45155e2f372f1328b955550ef3a9a9ed8ede690f
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-17 15:38:04 -0400

    finished test for place

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,12 +8,21 @@ describe Board do
     expect(board).to be_kind_of(Board)
   end
 
-  it "should return the cells in the dictionary" do
+  xit "should return the cells in the dictionary" do
     expect(board.cells).to eq({})
   end
 
   it "should return true if a cell is on the board" do
-    expect(board.valid_coordinate?("A1")).to eq (true)
-    expect(board.valid_coordinate?("A222")).to eq (false)
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("A222")).to eq(false)
+  end
+
+  it "should return the ship in the cell" do
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    expect(board.cells["A1"]).to eq(cruiser)
+    expect(board.cells["A2"]).to eq(cruiser)
+    expect(board.cells["A3"]).to eq(cruiser)
   end
 end
