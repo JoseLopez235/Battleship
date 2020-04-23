a950c9c87668a73796832f8a3df0f02d35dd572b
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-23 12:00:19 -0400

    created runner tests for MM

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -1,5 +1,7 @@
 require "rspec/autorun"
 require_relative "../lib/runner.rb"
+require_relative "../lib/board.rb"
+require_relative "../lib/ship.rb"
 
 describe Runner do
   let(:runner) {Runner.new}
@@ -8,8 +10,15 @@ describe Runner do
     expect(runner).to be_kind_of(Runner)
   end
 
-
-  xit "should return true" do
+  xit "should return true if Y in main menu" do
     expect(runner.play).to eq(true)
+  end
+
+  it "should return the board size" do
+    expect(runner.choice_conversion).to eq(4)
+  end
+
+  xit "should return an array with ships" do
+    expect(runner.ships[0]).to be_kind_of(Ship)
   end
 end
