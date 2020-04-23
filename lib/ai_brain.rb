944a97976984e6bbeede3b4f469f9e9209af83a7
Kevin Cuadros <kev040798@gmail.com>
2020-04-23 18:30:07 -0400

    Needs to be worked on.

--- lib/ai_brain.rb
+++ lib/ai_brain.rb
@@ -1,3 +1,9 @@
+=begin
+At this point in A.I were tring to create the smart approach for the
+the A.I to play against the user. Right now we were able to separate the
+surrounding coordinates with the ship. We need to make sure if the ship was sunk
+immediately.
+=end
 require_relative 'board.rb'
 require_relative 'ship.rb'
 require_relative 'cell.rb'
@@ -8,6 +14,7 @@ class ArtificialIntelligence
     @hunt = nil
     @surroundcells = []
     @unused_cells = cells.dup
+    @destroy = true
   end
 
   def hunt_cell
@@ -19,9 +26,7 @@ class ArtificialIntelligence
 
     if !random_code.empty?
       @hunt = random_code
-      return true
-    else
-      return false
+      @destroy = false
     end
   end
 
@@ -47,5 +52,18 @@ class ArtificialIntelligence
     right = huntcord.next
     @surroundcells << cells.cells[left] if cells.valid_coordinate?(left)
     @surroundcells << cells.cells[right] if cells.valid_coordinate?(right)
+  end
+
+  def logical_strikes
+    ship_coord = []
+    ship_coord << @hunt
+    @surroundcells.each do |ele|
+      ele.fire_upon
+      ship_coord << ele if !ele.empty?
+    end
+    if @hunt.ship.sunk?
+      @destroy = true
+
+    end
   end
 end
