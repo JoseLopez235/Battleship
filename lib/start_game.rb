37d903576ef89a8a463849633d00c51de1cb78d3
Kevin Cuadros <kev040798@gmail.com>
2020-04-24 01:09:07 -0400

    main_menu

---
+++ lib/start_game.rb
@@ -0,0 +1,94 @@
+require_relative "board.rb"
+require_relative "ship.rb"
+
+class StartGame
+  def initialize()
+    @player = Board.new
+    @ai = Board.new
+    @name_of_ships = {
+      2 => "submarine",
+      3 => "cruiser",
+    }
+    main_menu
+  end
+
+  def main_menu
+    # Welcome to Battleship Functionality
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    user = gets.chomp
+    # should end here if q
+    until user.downcase == "p"
+      return main_menu
+    end
+    # should call the computer_ship_place method and player_ship_prompt
+    @player.computer_ship_place
+    @player.player_ship_prompt
+    # call main_menu
+    main_menu
+  end
+
+  def computer_ship_place
+    # the actual complicated method.
+  end
+
+  def player_ship_prompt
+    # welcome message
+    # calls generate_ships
+    # loop of the generate_ships to go in player ship input
+    # calls player_ship_input
+    # player or ai board
+    # turn(player)
+  end
+
+  def generate_ships(quantity=2, sizes=3)
+    # quantity => 3, sizes => [2,3,3],
+    # creates amount of ships told
+  end
+
+  def player_ship_input(ship)
+    # ship => Ship class
+    # gets input
+    # if valid calls player_ship_placement
+    # not valid calls player_ship_input(ship)
+  end
+
+  def player_ship_placement
+    # actually creating the ship class and saving it in the board
+  end
+
+  def valid_sequence?
+    # might take an argument
+    #return true if not valid
+  end
+
+  def player_turn
+    # prompt input and validated immediately
+    # not valid call player_turn again
+    # valid hit
+  end
+
+  def player_attack(coord)
+    # record the attack
+    # display_info
+    # if won return you won
+    # if hasnt won call ai_turn
+  end
+
+  def display_info(cell)
+    # check miss or hit
+    # check if sunk?
+  end
+
+  def ai_turn
+    # record the attack
+    # display_info
+    # if won return you won
+    # if hasnt won call player_turn
+  end
+
+  def validate_hit?(coord)
+    # validates coord
+  end
+
+end
