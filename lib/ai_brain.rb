75cf14beb9742641f8afccc0dd14d6d3e4b2d3d3
Miguel Silva <msilvaale08@gmail.com>
2020-04-23 23:10:28 -0400

    logical hits method

--- lib/ai_brain.rb
+++ lib/ai_brain.rb
@@ -9,12 +9,22 @@ require_relative 'ship.rb'
 require_relative 'cell.rb'
 
 class ArtificialIntelligence
-  attr_accessor :hunt, :unused_cells
+  attr_accessor :hunt, :unused_cells, :cells
   def initialize(cells)
+    @cells = cells
     @hunt = nil
+    @moving_direction = nil
+    @previous_hits = []
+    @move_succesful = false
     @surroundcells = []
     @unused_cells = cells.dup
     @destroy = true
+    @opposite_move = {
+      "left" => "right",
+      "right" => "left",
+      "up" => "down",
+      "down" => "up",
+    }
   end
 
   def hunt_cell
@@ -55,15 +65,103 @@ class ArtificialIntelligence
   end
 
   def logical_strikes
-    ship_coord = []
-    ship_coord << @hunt
-    @surroundcells.each do |ele|
-      ele.fire_upon
-      ship_coord << ele if !ele.empty?
+    if !@move_succesful
+      rand_index = rand(@surroundcells.length)
+      cur_hit = @surroundcells[rand_index]
+      cur_hit.fire_upon
+      return cur_hit.empty? ? @surroundcells.delete_at(rand_index) : hit_setup(cur_hit)
     end
-    if @hunt.ship.sunk?
-      @destroy = true
+    calc_next_hit
+  end
 
+  def hit_setup(cur_hit)
+    if cur_hit.ship.sunk?
+      return reset_vars
     end
+    @move_succesful = true
+    determine_direction(cur_hit)
+    @previous_hits << cur_hit
+    return cur_hit
+  end
+
+  def determine_direction(cur_hit)
+    if @hunt.coordinate[0] == cur_hit.coordinate[0]
+      @moving_direction = @hunt.coordinate[1] > cur_hit.coordinate[1] ? "left" : "right"
+      @surroundcells = probable_coord(0, cur_hit)
+    else
+      @moving_direction = @hunt.coordinate[0] > cur_hit.coordinate[0] ? "up" : "down"
+      @surroundcells = probable_coord(1, cur_hit)
+    end
+  end
+
+  def probable_coord(index, cur_hit)
+    @surroundcells.keep_if do |coord|
+      @hunt.coordinate[index] == coord.coordinate[index] && cur_hit.coordinate != coord.coordinate
+    end
+  end
+
+  def calc_next_hit
+    next_hit = determine_next_hit
+    next_hit.fire_upon
+    @previous_hits << next_hit
+    if next_hit.ship.sunk?
+      reset_vars
+    elsif next_hit.empty?
+      miss_hit
+    end
+    return next_hit
+  end
+
+  def miss_hit
+    @moving_direction = @opposite_move[@moving_direction]
+    @move_succesful = false
+  end
+
+  def determine_next_hit
+    case @moving_direction
+    when "left"
+      return left_move
+    when "right"
+      return right_move
+    when "up"
+      return up_move
+    when "down"
+      return down_move
+    end
+  end
+
+  def left_move(cur_hit=@previous_hits[-1])
+    @moving_direction = "left"
+    next_hit = cur_hit.coordinate[0] << (cur_hit.coordinate[1].to_i - 1).to_s
+    return right_move(@hunt) if !@cells[next_hit]
+    return @cells[next_hit].fired_upon? ? right_move(@hunt) : @cells[next_hit]
+  end
+
+  def right_move(cur_hit=@previous_hits[-1])
+    @moving_direction = "right"
+    next_hit = cur_hit.coordinate.next
+    return left_move(@hunt) if !@cells[next_hit]
+    return @cells[next_hit].fired_upon? ? left_move(@hunt) : @cells[next_hit]
+  end
+
+  def down_move(cur_hit=@previous_hits[-1])
+    @moving_direction = "down"
+    next_hit = cur_hit.coordinate[0].next << cur_hit.coordinate[1]
+    return up_move(@hunt) if !@cells[next_hit]
+    return @cells[next_hit].fired_upon? ? up_move(@hunt) : @cells[next_hit]
+  end
+
+  def up_move(cur_hit=@previous_hits[-1])
+    @moving_direction = "up"
+    next_hit = (cur_hit.coordinate[0].ord - 1).chr << cur_hit.coordinate[1]
+    return down_move(@hunt) if !@cells[next_hit]
+    return @cells[next_hit].fired_upon? ? down_move(@hunt) : @cells[next_hit]
+  end
+
+  def reset_vars
+    @moving_direction = nil
+    @previous_hits = []
+    @move_succesful = false
+    @destroy = true
   end
 end
