e81af8d148c092d3ac20bcc59920d4c8d82a4a81
Roberto Basulto <roberto.basulto085@gmail.com>
2020-04-24 15:18:15 -0400

    test for player ship prompt

--- test/start_game_test.rb
+++ test/start_game_test.rb
@@ -6,22 +6,22 @@ describe StartGame do
   let(:start_game) {StartGame.new}
   let(:ship_one) {Ship.new("cruiser", 3)}
 
-  it "should exist" do
+  xit "should exist" do
     expect(start_game).to be_kind_of(StartGame)
   end
 
-  it "should return generate a ship" do
+  xit "should return generate a ship" do
     expect(start_game.generate_ships.length).to eq(2)
     expect(start_game.generate_ships[0]).to be_kind_of(Ship)
     expect(start_game.generate_ships[1]).to be_kind_of(Ship)
   end
 
-  it "should return true if coord is valid" do
+  xit "should return true if coord is valid" do
     expect(start_game.valid_sequence?(ship_one, ["A1", "A2", "A3"])).to eq(true)
     expect(start_game.valid_sequence?(ship_one, ["Z1", "Z2", "Z3"])).to eq(false)
   end
 
-  it "should return a ship in a cell" do
+  xit "should return a ship in a cell" do
     start_game.player_ship_placement(ship_one, ["A1", "A2", "A3"])
 
     expect(start_game.player.cells["A1"].ship).to eq(ship_one)
@@ -29,10 +29,16 @@ describe StartGame do
     expect(start_game.player.cells["A3"].ship).to eq(ship_one)
   end
 
-  it "should call player_ship_placement" do
+  xit "should call player_ship_placement" do
     allow(start_game).to receive(:gets) { "A1 A2 A3" }
     expect(start_game).to receive(:player_ship_placement).with(ship_one, ["A1", "A2", "A3"])
     start_game.player_ship_input(ship_one)
   end
 
+  it "should call player ship prompt and make shure a ship is placed" do
+    start_game.player_ship_prompt
+    
+    expect(start_game.player.cells["A1"].ship).to be_kind_of(Ship)
+    expect(start_game.player.cells["A2"].ship).to be_kind_of(Ship)
+  end
 end
