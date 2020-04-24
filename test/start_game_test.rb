d894af1a3a1864a5ac49f0a3cb11608ee1f2660f
Miguel Silva <msilvaale08@gmail.com>
2020-04-24 00:26:25 -0400

    finished ship placement

--- test/start_game_test.rb
+++ test/start_game_test.rb
@@ -4,14 +4,24 @@ require_relative "../lib/ship.rb"
 
 describe StartGame do
   let(:start_game) {StartGame.new}
+  let(:ship_one) {Ship.new("cruiser", 3)}
 
   it "should exist" do
     expect(start_game).to be_kind_of(StartGame)
   end
 
-  it "should return a ship in a cell" do
+  it "should return generate a ship" do
     expect(start_game.generate_ships.length).to eq(2)
     expect(start_game.generate_ships[0]).to be_kind_of(Ship)
     expect(start_game.generate_ships[1]).to be_kind_of(Ship)
   end
+
+  it "should return a ship in a cell" do
+    start_game.player_ship_placement(ship_one, ["A1", "A2", "A3"])
+
+    expect(start_game.player.cells["A1"].ship).to eq(ship_one)
+    expect(start_game.player.cells["A2"].ship).to eq(ship_one)
+    expect(start_game.player.cells["A3"].ship).to eq(ship_one)
+  end
+
 end
